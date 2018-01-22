592ec65fbe3d60ff64e48c11368927973e52d264
Alan Mills <aelonrench@yahoo.com>
2018-01-22 16:57:54 +0000

    Enable change guest name in state

--- src/GuestList.js
+++ src/GuestList.js
@@ -10,14 +10,16 @@ const GuestList = props =>
         isConfirmed={guest.isConfirmed}
         isEditing={guest.isEditing} 
         handleConfirmation={() => props.toggleconfirmationAt(index)}
-        handleToggleEditing={() => props.toggleEditingAt(index)} />
+        handleToggleEditing={() => props.toggleEditingAt(index)} 
+        setName={text => props.setNameAt(text, index)} />
       )}
     </ul>;
 
 GuestList.propTypes = {
   guests: PropTypes.array.isRequired,
   toggleconfirmationAt: PropTypes.func.isRequired,
-  toggleEditingAt: PropTypes.func.isRequired
+  toggleEditingAt: PropTypes.func.isRequired,
+  setNameAt: PropTypes.func.isRequired
 }
 
 
